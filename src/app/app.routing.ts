5a8049be155c1bf968b5d88b9ab3c0a7275d4354
reda chanaa <chanaa-reda@hotmail.fr>
2021-04-21 12:54:27 +0200

    Update nodeModules

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -38,8 +38,9 @@ const routes: Routes =[
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes,{
-       useHash: true
-    })
+    useHash: true,
+    relativeLinkResolution: 'legacy'
+})
   ],
   exports: [
   ],
